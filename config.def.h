d15cad71711fe7948badb113061c725e00d1627e
Hamza Zarrouk <hamzazarouk@gmail.com>
2022-12-01 17:12:19 +0100

    patched the fuck up of it

--- config.def.h
+++ config.def.h
@@ -10,10 +10,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#a9b1d6", "#1a1b26" },
+	[SchemeSel] = { "#1a1b26", "#0db9d7" },
+	[SchemeSelHighlight] = { "#1a1b26", "#0db9d7" },
+	[SchemeNormHighlight] = { "#b9f27c", "#1a1b26" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
